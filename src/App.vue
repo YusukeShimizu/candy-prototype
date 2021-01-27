f93ac1d985a7535e977cfd9cc83f78e8d56c9cc1
bruwbird <stm1051212@yahoo.co.jp>
2021-01-27 10:46:25 +0900

    input my message

--- src/App.vue
+++ src/App.vue
@@ -2,34 +2,18 @@
   <v-app>
     <v-app-bar app color="primary" dark>
       <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
+        <v-icon>mdi-monitor-speaker</v-icon>
       </div>
 
       <v-spacer></v-spacer>
 
       <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        href="https://github.com/YusukeShimizu/candy-prototype"
         target="_blank"
         text
       >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
+        <span class="mr-2">github</span>
+        <v-icon>mdi-github</v-icon>
       </v-btn>
     </v-app-bar>
 
